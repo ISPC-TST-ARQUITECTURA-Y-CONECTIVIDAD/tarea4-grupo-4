0f106b98c03bb72657d018adc95a94ce1093e7f1
Fernando Vexenat <106171748+fvexe82@users.noreply.github.com>
2023-05-15 11:45:10 -0300

    Se agrega punto 4
    Co-authored-by: Mayte2008 <caronis22@gmail.com>

---
+++ Punto 4/Arduino_Json.cpp
@@ -0,0 +1,54 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <ArduinoJson.h>
+
+#define DHTPIN 2 //
+
+#define DHTTYPE DHT11 // DHT 11
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop()
+{
+  delay(1500);
+
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature))
+  {
+    Serial.println(F("Error de lectura de temperatura"));
+  }
+  else
+  {
+    Serial.print(F("Temperatura: "));
+    Serial.print(event.temperature);
+    Serial.println(F(" °C"));
+	float temperatura = dht.readTemperature();
+  }
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity))
+  {
+    Serial.println(F("Error de lectura de humedad"));
+  }
+  else
+  {
+    Serial.print(F("Humedad: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+	float humedad = dht.readHumidity();
+  }
+  StaticJsonDocument<200> doc;
+  doc["Temperatura:"] = temperatura;
+  doc["Humedad:"] = humedad;
+  
+  serializeJson(doc, Serial);
+  Serial.println();
+  
+  delay(1000);
+}
